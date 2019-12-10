da3b693b787492834a4796bfabf94372fc8af233
qianhuisun <qianhuisun2020@u.northwestern.edu>
2019-12-09 22:16:45 -0600

    bug fixed: package loss during receiving

--- client_test.py
+++ client_test.py
@@ -1,25 +1,29 @@
 from socket import *
+import os
 
 glassClientSocket = socket(AF_INET, SOCK_STREAM)
 glassPort = 8088
-# glassClientSocket.connect(('localhost', glassPort))
+#glassClientSocket.connect(('localhost', glassPort))
 glassClientSocket.connect(('3.134.84.232', glassPort))
 
 try:
-    imageFile = open("sample.jpg", 'rb')
-    imageBytes = imageFile.read()
-    size = len(imageBytes)
-    imageFile.close()
-
+    size = os.stat("sample.jpg").st_size
     glassClientSocket.sendall(("SIZE " + str(size)).encode())
-    response = glassClientSocket.recv(4096).decode()
+    response = glassClientSocket.recv(1024).decode()
 
     print("client response = " + response)
 
     if response == "GOT SIZE":
+        
+        imageFile = open("sample.jpg", 'rb')
+        sent_size = 0
+        while sent_size != size:
+            imageBytes = imageFile.read(1024)
+            glassClientSocket.sendall(imageBytes)
+            sent_size += len(imageBytes)
+        imageFile.close()
 
-        glassClientSocket.sendall(imageBytes)
-        response = glassClientSocket.recv(4096).decode()
+        response = glassClientSocket.recv(1024).decode()
 
         print("client response = " + response)
 
--- server_test.py
+++ server_test.py
@@ -5,6 +5,7 @@ from select import *
 import threading
 import _thread
 
+
 class Server(object):
 
     def __init__(self):
@@ -27,54 +28,66 @@ class Server(object):
     def startGlassServer(self):
         print("startGlassServer:")
         
+        self.glassServerSocket.setblocking(0)
         self.glassServerSocket.setsockopt(SOL_SOCKET, SO_KEEPALIVE, 1)
-        #self.glassServerSocket.bind(('localhost', self.glassPort))
+        #self.glassServerSocket.bind(("localhost", self.glassPort))
         self.glassServerSocket.bind((gethostname(), self.glassPort))
         print("GlassServerSocket binded to port: ", self.glassPort)
-        self.glassServerSocket.listen(1)
+        self.glassServerSocket.listen(5)
         print("GlassServerSocket is listening")
 
-        while True:
-            
-            status = input("listen to next client? (Y/N)")
+        inputs = [self.glassServerSocket]
 
-            if status == "n" or status == "N":
-                break
-            
-            if status != "y" and status != "Y":
-                continue
-            
-            conn, addr = self.glassServerSocket.accept()
+        while inputs:
+
+            if len(inputs) == 1:
+
+                status = input("listen to next client? (Y/N)")
+
+                if status == "n" or status == "N":
+                    break
                 
-            try:
-                print("handleGlassConnection:")
-                data = conn.recv(4096)
-                request = data.decode()
-                if request.startswith('SIZE'):
-                    strings = request.split()
-                    size = int(strings[1])
-                    print("Got size: " + str(size))
-                    conn.sendall("GOT SIZE".encode())
-                    data = b''
-                    chunk = conn.recv(4096)
-                    while len(chunk) == 4096:
-                        print("chunk size = " + str(len(chunk)))
-                        data += chunk
-                        chunk = conn.recv(4096)
-                    data += chunk
-                    if data:
-                        print("Got image")
-                        imageFile = open("received.png", 'wb')
-                        imageFile.write(data)
-                        imageFile.close()
-                        conn.sendall("GOT IMAGE".encode())
-                        conn.shutdown(SHUT_WR)
-            except:
-                conn.shutdown(SHUT_WR)
-                continue
+                if status != "y" and status != "Y":
+                    continue
+                
+            readable, writable, exceptional = select(inputs, [], inputs)
+
+            for s in exceptional:
+                inputs.remove(s)
+                s.close()
+            
+            for s in readable:
+                if s is self.glassServerSocket:
+                    conn, addr = s.accept()
+                    conn.setblocking(0)
+                    inputs.append(conn)
+                else:
+                    inputs.remove(s)
+                    s.setblocking(1)
+                    try:
+                        data = s.recv(1024)
+                        request = data.decode()
+                        if request.startswith('SIZE'):
+                            strings = request.split()
+                            size = int(strings[1])
+                            print("Got size: " + str(size))
+                            conn.sendall("GOT SIZE".encode())
+
+                            imageFile = open("received.jpg", 'wb')
+                            received_size = 0
+                            while received_size != size:
+                                data = s.recv(1024)
+                                imageFile.write(data)
+                                received_size += len(data)
+                            print("Got image")
+                            imageFile.close()
+                            conn.sendall("GOT IMAGE".encode())
+                        s.close()
+                    except Exception as e:
+                        print(e)
+                        s.close()
 
         self.glassServerSocket.close()
-
 
     def startPhoneServer(self):
         print("startPhoneServer:")
